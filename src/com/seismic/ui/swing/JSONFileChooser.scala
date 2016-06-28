361dcaf3245b077e7e2e08cff7d5cedd6d7da910
Dave Clay <daveclay@gmail.com>
2016-06-28 09:31:22 -0700

    Preferences to help with swing's shit file chooser

--- src/com/seismic/ui/swing/JSONFileChooser.scala
+++ src/com/seismic/ui/swing/JSONFileChooser.scala
@@ -4,12 +4,13 @@ import java.io.File
 import javax.swing.filechooser.FileNameExtensionFilter
 import javax.swing.{JFileChooser, JFrame}
 
+import com.seismic.utils.SetListSerializer
+
 class JSONFileChooser(frame: JFrame, fileSelected: (File) => Unit) {
 
   def show(): Unit = {
-    val chooser = new JFileChooser
-    val filter = new FileNameExtensionFilter("JSON Files", "json")
-    chooser.setFileFilter(filter)
+    val chooser = new JFileChooser(SetListSerializer.preferences.lastSetListDir)
+    chooser.setFileFilter(new FileNameExtensionFilter("JSON Files", "json"))
     if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
       fileSelected(chooser.getSelectedFile)
     }
--- src/com/seismic/utils/SetListSerializer.scala
+++ src/com/seismic/utils/SetListSerializer.scala
@@ -8,11 +8,36 @@ import com.seismic.SetList
 
 object SetListSerializer {
 
+  object Preferences {
+    val homeDir = System.getProperties.getProperty("user.dir")
+    val preferencesFile = new File(homeDir, ".seismic.json")
+
+    def buildPreferences() = {
+      if ( ! preferencesFile.exists()) {
+        val preferences = Preferences(homeDir)
+        objectMapper().writeValue(preferencesFile, preferences)
+        preferences
+      } else {
+        objectMapper().readValue(preferencesFile, classOf[Preferences])
+      }
+    }
+  }
+
+  val preferences = Preferences.buildPreferences()
+
+  case class Preferences(var lastSetListDir: String) {
+    def setLastSetListDirFromFile(lastSetListFile: File): Unit = {
+      this.lastSetListDir = lastSetListFile.getParentFile.getAbsolutePath
+    }
+  }
+
   def write(setList: SetList): Unit = {
     objectMapper().writeValue(new File(f"${setList.name}.json"), setList)
   }
 
   def read(file: File) = {
+    preferences.setLastSetListDirFromFile(file)
+    objectMapper().writeValue(Preferences.preferencesFile, preferences)
     objectMapper().readValue(file, classOf[SetList])
   }
 
